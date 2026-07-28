fa766307299a7b0c43ec3746f0f44e58d92b8f97
wangenga <daisyimg65@gmail.com>
2026-07-28 13:33:18 +0300

    Refactor Main class to replace ArrayList manipulation with StringConcatenator functionality, demonstrating string concatenation instead of element removal.

--- Main.java
+++ Main.java
@@ -1,19 +1,10 @@
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Double> list = new ArrayList<>();
-        list.add(1.5);
-        list.add(2.5);
-        list.add(3.5);
-        list.add(4.5);
-        list.add(5.5);
-
-        ArrayList<Double> result = ArrayModifier.removeElementsBetween(list, 1, 3);
-
-        for (double num : result) {
-            System.out.print(num + " ");
-        }
+        StringConcatenator stringConcatenator = new StringConcatenator();
+        String result = stringConcatenator.concatenate("Hello", ", ", "varArgs", "!");
+        System.out.println(result);
     }
+
 }
---
+++ StringConcatenator.java
@@ -0,0 +1,12 @@
+//package sprint;
+
+public class StringConcatenator {
+    public static String concatenate (String... arguments){
+        StringBuilder con = new StringBuilder();
+
+        for (String argument : arguments){
+            con.append(argument);
+        }
+        return con.toString();
+    }
+}
