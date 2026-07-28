5fb978f942707d16110986df743e29ca19e72c69
wangenga <daisyimg65@gmail.com>
2026-07-28 14:38:24 +0300

    Refactor Main class to replace string concatenation with array filtering functionality, demonstrating the use of ArrayFilter to filter a 2D array based on sum criteria.

---
+++ ArrayFilter.java
@@ -0,0 +1,20 @@
+//package sprint;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayFilter {
+    public int[][] filterBySum (int[][] x, int target){
+        List<int[]> z = new ArrayList<>();
+
+        for (int[] row : x){
+            int sum = 0;
+            for (int num: row){
+                sum =+ target;
+            }
+            if (sum < target){
+                z.add(row);
+            }
+        }
+        return z.toArray(new int [0][]);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        StringConcatenator stringConcatenator = new StringConcatenator();
-        String result = stringConcatenator.concatenate("Hello", ", ", "varArgs", "!");
-        System.out.println(result);
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            {1, 1, 1}
+        };
+
+        ArrayFilter arrayFilter = new ArrayFilter();
+        int[][] filteredArray = arrayFilter.filterBySum(array, 10);
+
+        for (int[] row : filteredArray) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 
 }
