4a167304a8722627a2bc1d7c5a5e6f1075ff1cf2
wangenga <daisyimg65@gmail.com>
2026-07-29 12:27:23 +0300

    Refactor Main class to replace number processing with word length analysis functionality, demonstrating the analysis of word lengths from a list of strings and outputting the result.

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        NumberProcessor processor = new NumberProcessor();
-        List<Integer> numbers = Arrays.asList(3, 5, 12, 20, 7, 10);
-
-        Optional<Integer> result = processor.processNumbers(numbers);
+        WordLengthAnalyzer analyzer = new WordLengthAnalyzer();
+        Map<Integer, Integer> result = analyzer.analyzeWordLengths(Arrays.asList("apple", "banana", "cherry", "date", "fig"));
         System.out.println(result);
     }
-
 }
---
+++ WordLengthAnalyzer.java
@@ -0,0 +1,12 @@
+package sprint;
+
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
+
+public class WordLengthAnalyzer {
+    public Map<Integer, Integer> analyzeWordLengths(List<String> words) {
+        return words.stream()
+            .collect(Collectors.groupingBy(String::length, Collectors.summingInt(word -> 1)));
+    }
+}
