98a9ff8e6845405289b225dabe873de89a4fd992
wangenga <daisyimg65@gmail.com>
2026-07-29 07:47:04 +0300

    Refactor Main class to replace 2D array filtering with number processing functionality using StreamPolice, demonstrating list processing and output of results.

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[][] array = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9},
-            {1, 1, 1}
-        };
-
-        ArrayFilter arrayFilter = new ArrayFilter();
-        int[][] filteredArray = arrayFilter.filterBySum(array, 10);
-
-        for (int[] row : filteredArray) {
-            System.out.println(Arrays.toString(row));
-        }
+        StreamPolice processor = new StreamPolice();
+        List<Integer> numbers = Arrays.asList(-10, 15, 20, 30, 8, -3, 5, 10, 50, 12);
+        List<Integer> result = processor.processNumbers(numbers);
+        System.out.println(result);
     }
-
 }
---
+++ StreamPolice.java
@@ -0,0 +1,13 @@
+package sprint;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamPolice {
+    public List<Integer> processNumbers(List<Integer> x){
+        return x.stream()
+            .filter(num -> num > 0 )
+            .filter(num -> !(num % 5 == 0 && num % 10 != 0))
+            .collect(Collectors.toList());
+    }
+}
