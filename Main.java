1522f65c050c3f92aa18a82702cdc3f4602bd2f6
wangenga <daisyimg65@gmail.com>
2026-07-30 22:21:31 +0300

    Refactor Main class to replace occurrence counting functionality with parentheses balance checking, introducing ParenthesesBalanceChecker class to validate balanced parentheses in strings.

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        OccurrenceCounter counter = new OccurrenceCounter();
-        int[] arr = {1, 2, 3, 2, 4, 2, 5};
-        System.out.println(counter.countOccurrences(arr, 2, 0));
-        System.out.println(counter.countOccurrences(arr, 6, 0));
-        System.out.println(counter.countOccurrences(null, 1, 0));
-        System.out.println(counter.countOccurrences(new int[]{}, 1, 0));
+        ParenthesesBalanceChecker checker = new ParenthesesBalanceChecker();
+        System.out.println(checker.isBalanced("(())"));
+        System.out.println(checker.isBalanced("(a()b()c)"));
+        System.out.println(checker.isBalanced("(()"));
+        System.out.println(checker.isBalanced(")("));
+        System.out.println(checker.isBalanced(""));
+        System.out.println(checker.isBalanced(null));
     }
 
 }
---
+++ ParenthesesBalanceChecker.java
@@ -0,0 +1,40 @@
+package sprint;
+
+public class ParenthesesBalanceChecker {
+    public boolean isBalanced(String str) {
+        if (str == null){
+            return false;
+        }
+
+        str = str.replaceAll("[^()]", "");
+
+        if (str.length() % 2 != 0 ){
+            return false;
+        }
+        
+        return checkBalance(str, 0, 0);
+        
+
+    }
+
+    private boolean checkBalance(String str, int index, int balance) {
+        
+        if(balance < 0){
+            return false;
+        }
+        
+        if (balance == 0 && str.length() == index ){
+            return true;
+        }
+        
+        if (str.charAt(index) == '('){
+                balance = balance + 1;
+            }
+
+            else if (str.charAt(index) == ')'){
+                balance = balance - 1;
+            } 
+
+        return checkBalance(str, index + 1, balance);
+    }
+}
