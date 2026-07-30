a7996b185c118dd35b4416606351372523e345ca
wangenga <daisyimg65@gmail.com>
2026-07-30 11:48:10 +0300

    Refactor Main class to replace weekend day calculation with palindrome checking functionality, and implement PalindromeChecker class to determine if strings are palindromes.

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-import java.time.LocalDate;
-
 public class Main {
 
     public static void main(String[] args) {
-        LocalDate startDate = LocalDate.of(2024, 8, 1);
-        LocalDate endDate = LocalDate.of(2024, 8, 31);
-        WeekendCalculator calculator = new WeekendCalculator();
-        long weekendDayCount = calculator.countWeekendDays(startDate, endDate);
-        System.out.println(weekendDayCount);
+        String testString1 = "A man, a plan, a canal, Panama";
+        String testString2 = "radar ";
+        System.out.println(PalindromeChecker.isPalindrome(testString1));
+        System.out.println(PalindromeChecker.isPalindrome(testString2));
     }
 
 }
--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,5 +1,13 @@
-//package sprint;
+package sprint;
 
 public class PalindromeChecker {
+    public static boolean isPalindrome(String x){
+        if (x == null) return false;
+
+        String clean = x.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        String reverse = new StringBuilder(clean).reverse().toString();
+        return clean.equals(reverse);
+    }
     
 }
