737ed6de0512a10108f7bf1118392f402f6407c0
wangenga <daisyimg65@gmail.com>
2026-07-30 13:57:47 +0300

    Update Factorial class to correctly handle input values by returning 0 for negative numbers and returning 1 for both 0 and 1, ensuring accurate factorial calculations.

--- Factorial.java
+++ Factorial.java
@@ -1,11 +1,11 @@
 package sprint;
 public class Factorial {
     public int calculateFactorial(int n){
-        if (n < 1){
+        if (n < 0){
             return 0;
         }
 
-        if (n == 1){
+        if (n == 1 || n == 0){
             return 1;
         }
 
